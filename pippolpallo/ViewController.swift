5ed0b98739b1df0ce33e57fdc9f4699a40997588
Cortese Marco <cortesemarco@san-19.local>
2017-09-28 12:40:05 +0200

    hvv

--- pippolpallo/ViewController.swift
+++ pippolpallo/ViewController.swift
@@ -4,27 +4,3 @@
 //
 //  Created by Mauriello Lorenzo on 28/09/2017.
 //  Copyright © 2017 Mauriello Lorenzo. All rights reserved.
-//
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("gdsayufuy")
-        
-        
-        print("jghfxdgguhijogfd")
-        
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
-
